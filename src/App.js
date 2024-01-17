6b1242b758b664c14bf72f2f0563ad86ce4c1bcb
Yolisma <yolisisabel28@gmail.com>
2024-01-17 14:21:55 -0800

    initial setup however cannot render course titles

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+//import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const [courses, setCourses] = useState([]);
+
+  useEffect(() =>{
+    fetch('https://localhost:5000/api/courses')
+      .then(res => res.json())
+      .then(data => console.log(data)
+  });
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <>
+    <ul>
+      {
+      courses.map(course => <li>{
+      courses.title
+    }</li>)}</ul>
+    </>
+
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
   );
 }
 
 export default App;
+
+//create a get req to server 
+//server is located localost:5000
+//req, res
+//return list of courses
+//iterate through list of courses by using loop
+//return title of each one 
+//map
