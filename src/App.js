e66e251baa40062c0cad52be7af2e523e68f7b14
Yolisma <yolisisabel28@gmail.com>
2024-01-17 16:11:46 -0800

    rendered courses data on console

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 //import logo from "./logo.svg";
 import "./App.css";
+import { useState, useEffect } from "react";
 
 function App() {
   const [courses, setCourses] = useState([]);
 
-  useEffect(() =>{
-    fetch('https://localhost:5000/api/courses')
-      .then(res => res.json())
-      .then(data => console.log(data)
-  });
-  
-  return (
+  useEffect(() => {
+    fetch("http://localhost:5000/api/courses")
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  }, []);
 
+  return (
     <>
-    <ul>
-      {
-      courses.map(course => <li>{
-      courses.title
-    }</li>)}</ul>
+      <ul>
+        {courses.map((course) => (
+          <li>{courses.title}</li>
+        ))}
+      </ul>
     </>
 
     // <div className="App">
@@ -41,10 +41,10 @@ function App() {
 
 export default App;
 
-//create a get req to server 
+//create a get req to server
 //server is located localost:5000
 //req, res
 //return list of courses
 //iterate through list of courses by using loop
-//return title of each one 
+//return title of each one
 //map
