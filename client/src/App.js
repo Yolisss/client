239977a2e6937bc37fe31f38d5f041e0a3d3a03e
Yolisma <yolisisabel28@gmail.com>
2024-01-18 15:01:19 -0800

    parsed res body to JSON

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,7 @@ function App() {
 
   useEffect(() => {
     fetch("http://localhost:5000/api/courses")
-      .then((res) => console.log(res))
+      .then((res) => res.json())
       .then((data) => console.log(data));
   }, []);
 
