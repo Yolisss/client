262dbc8a1f763a1ebc59505930b2ced8f5712116
Yolisma <yolisisabel28@gmail.com>
2024-01-18 22:41:30 -0800

    create route for user sign in

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,14 @@ import "./App.css";
 //import { useState, useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
 import Courses from "./components/Courses";
+import UserSignIn from "./components/UserSignIn";
 
 function App() {
   return (
     <div>
       <Routes>
         <Route path="/" element={<Courses />} />
+        <Route path="/signin" element={<UserSignIn />} />
       </Routes>
     </div>
   );
