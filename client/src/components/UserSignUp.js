b447dc09cbfde976f2fd33870bed26c165ea24c8
Yolisma <yolisisabel28@gmail.com>
2024-01-19 15:29:52 -0800

    successfully added new user to db

--- client/src/components/UserSignUp.js
+++ client/src/components/UserSignUp.js
@@ -1,29 +1,95 @@
+import { useRef } from "react";
+import { Link, useNavigate, useLocation } from "react-router-dom";
+import { useState } from "react";
+
 const UserSignUp = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const firstName = useRef(null);
+  const lastName = useRef(null);
+  const emailAddress = useRef(null);
+  const password = useRef(null);
+  const [errors, setErrors] = useState([]);
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    const user = {
+      firstName: firstName.current.value,
+      lastName: lastName.current.value,
+      emailAddress: emailAddress.current.value,
+      password: password.current.value,
+    };
+
+    const fetchOptions = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json; charset=utf-8",
+      },
+      body: JSON.stringify(user),
+    };
+
+    const response = await fetch(
+      "http://localhost:5000/api/users",
+      fetchOptions
+    );
+    console.log(response);
+    if (response.status === 201) {
+      console.log(
+        `${user.firstName} ${user.lastName} is successfully signed up and authenticated`
+      );
+    } else if (response.status === 400) {
+      const data = await response.JSON();
+      console.log(data);
+    }
+  };
+
+  const handleCancel = (event) => {
+    event.preventDefault();
+    navigate("/");
+  };
   return (
     <div>
       <div className="form--centered">
         <h2>Sign Up</h2>
-        <form>
+        <form onSubmit={handleSubmit}>
           <label htmlFor="firstName">First Name</label>
-          <input id="firstName" name="firstName" type="text" defaultValue />
+          <input
+            id="firstName"
+            name="firstName"
+            type="text"
+            defaultValue
+            ref={firstName}
+          />
           <label htmlFor="lastName">Last Name</label>
-          <input id="lastName" name="lastName" type="text" defaultValue />
+          <input
+            id="lastName"
+            name="lastName"
+            type="text"
+            defaultValue
+            ref={lastName}
+          />
           <label htmlFor="emailAddress">Email Address</label>
           <input
             id="emailAddress"
             name="emailAddress"
             type="email"
             defaultValue
+            ref={emailAddress}
           />
           <label htmlFor="password">Password</label>
-          <input id="password" name="password" type="password" defaultValue />
+          <input
+            id="password"
+            name="password"
+            type="password"
+            defaultValue
+            ref={password}
+          />
           <button className="button" type="submit">
             Sign Up
           </button>
-          <button
-            className="button button-secondary"
-            onclick="event.preventDefault(); location.href='index.html';"
-          >
+          <button className="button button-secondary" onClick={handleCancel}>
             Cancel
           </button>
         </form>
