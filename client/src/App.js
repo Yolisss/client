bbe0f1a079eacbce5575588054626ef98b24e891
Yolisma <yolisisabel28@gmail.com>
2024-01-19 18:04:32 -0800

    fixed pathway for courseDetail

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import { Routes, Route } from "react-router-dom";
 import Courses from "./components/Courses";
 import UserSignIn from "./components/UserSignIn";
 import UserSignUp from "./components/UserSignUp";
+import CourseDetail from "./components/CourseDetail";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path="/" element={<Courses />} />
         <Route path="/signin" element={<UserSignIn />} />
         <Route path="/signup" element={<UserSignUp />} />
+        <Route path="/courses/:id" element={<CourseDetail />} />
       </Routes>
     </div>
   );
