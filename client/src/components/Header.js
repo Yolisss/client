f033ef529421682ebc32bc163cbd7fcc3c5b271f
Yolisma <yolisisabel28@gmail.com>
2024-01-20 22:48:12 -0800

    created header component

---
+++ client/src/components/Header.js
@@ -0,0 +1,37 @@
+import { Link } from "react-router-dom";
+import { useContext } from "react";
+import UserContext from "../context/UserContext";
+const Header = () => {
+  const { authUser } = useContext(UserContext);
+  return (
+    <header>
+      <div className="wrap header--flex">
+        <h1 className="header--logo">
+          <Link to="/">Courses</Link>
+        </h1>
+        <nav>
+          {authUser === null ? (
+            <ul className="header--signedout">
+              <li>
+                <Link to="/signup">Sign Up</Link>
+              </li>
+              <li>
+                <Link to="/signin">Sign In</Link>
+              </li>
+            </ul>
+          ) : (
+            <ul className="header--signedin">
+              <li>
+                Welcome, {authUser.firstName} {authUser.lastName}
+              </li>
+              <li>
+                <Link to="/signout">Sign Out</Link>
+              </li>
+            </ul>
+          )}
+        </nav>
+      </div>
+    </header>
+  );
+};
+export default Header;
