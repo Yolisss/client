f6b240a4a9d37020febc434b504961e7ee31fbe5
Yolisma <yolisisabel28@gmail.com>
2024-01-20 22:50:47 -0800

    created signout component and route

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,7 @@ import CourseDetail from "./components/CourseDetail";
 import CreateCourse from "./components/CreateCourse";
 import UpdateCourse from "./components/UpdateCourse";
 import Header from "./components/Header";
+import UserSignOut from "./components/UserSignOut";
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
         <Route path="/" element={<Courses />} />
         <Route path="/signin" element={<UserSignIn />} />
         <Route path="/signup" element={<UserSignUp />} />
+        <Route path="/signout" element={<UserSignOut />} />
         <Route path="/courses/:id" element={<CourseDetail />} />
         <Route path="/courses/create" element={<CreateCourse />} />
         <Route path="/courses/:id/update" element={<UpdateCourse />} />
