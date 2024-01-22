ad468ac28ff0b17677f78a814b9845aa5eecfb71
Yolisma <yolisisabel28@gmail.com>
2024-01-22 10:39:30 -0800

    created signin and signout for user authentication

--- client/src/context/UserContext.js
+++ client/src/context/UserContext.js
@@ -1,45 +1,50 @@
 import { createContext, useState } from "react";
 import Cookies from "js-cookie";
-import { api } from "../utils/apiHelper";
 
+// initialize new context
 const UserContext = createContext(null);
 
-//provides data throughout the app
-//in index.js, app component is wrapped in UserProvider tags
-//all components in app can get access to the data we pass to UserContext
 export const UserProvider = (props) => {
-  //if cookies.get finds a cookie name authenticatedUser
-  //then cookie will equal the value stored
-  //if there's no authenticatedUser cookie, cookie = null
+  // use cookie to preserve user's state
   const cookie = Cookies.get("authenticatedUser");
   const [authUser, setAuthUser] = useState(cookie ? JSON.parse(cookie) : null);
+  // Handle sign in
   const signIn = async (credentials) => {
-    //6.
-    const response = await api("/users", "GET", null, credentials);
+    // use btoa method to encode user credentials
+    const encodedCredentials = btoa(
+      `${credentials.emailAddress}:${credentials.password}`
+    );
+    // Pass credentials into authorization header using basic authentication scheme
+    const fetchOptions = {
+      method: "GET",
+      headers: {
+        Authorization: `Basic ${encodedCredentials}`,
+      },
+    };
+    // Send GET request to get user's info
+    const response = await fetch(
+      "http://localhost:5000/api/users",
+      fetchOptions
+    );
+    // If user is successfully authenticated
     if (response.status === 200) {
-      //7. storing user's info
-      //to get access to the user's info from response, we'll
-      //need to parse response's body text to json
+      // store user's info
       const user = await response.json();
+      user.password = credentials.password;
+      // Set authorized user as the current user
       setAuthUser(user);
-      //store authenticated data
-      //first arg: specifies the name of the cookie set
-      //sec: string value to store in the cookie
-      //third: how long to store the data
-      //authenticatedUser cookie is set and valid across the entire app
-      //CREATE COOKIES
       Cookies.set("authenticatedUser", JSON.stringify(user), { expires: 1 });
       return user;
     } else if (response.status === 401) {
       return null;
     } else {
-      //if status is not a 200 or 401
       throw new Error();
     }
   };
+  // Handle sign out
   const signOut = () => {
+    // clear user's data
     setAuthUser(null);
-    //DELETE COOKIES
     Cookies.remove("authenticatedUser");
   };
   return (
