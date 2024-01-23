43af47a7af2b5ff67edf9b9a3c4f5613572dd137
Yolisma <yolisisabel28@gmail.com>
2024-01-23 09:42:41 -0800

    removed useNavigate to remove warning sign in console

--- client/src/components/CourseDetail.js
+++ client/src/components/CourseDetail.js
@@ -1,5 +1,5 @@
 import { useState, useEffect, useContext } from "react";
-import { Link, useParams, useNavigate } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import UserContext from "../context/UserContext";
 import ReactMarkdown from "react-markdown";
 
