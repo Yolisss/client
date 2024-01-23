d7e4d4f610bc84c33f94442bbc30942c7a96a004
Yolisma <yolisisabel28@gmail.com>
2024-01-23 07:51:29 -0800

    comment out css imports

--- client/src/index.js
+++ client/src/index.js
@@ -6,6 +6,9 @@ import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 import { UserProvider } from "./context/UserContext";
 
+// import "./styles/reset/css";
+// import "./styles/global.css";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
