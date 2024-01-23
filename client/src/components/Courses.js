611a9d8f9cae6271f00e711636210e0c8a1f3a8d
Yolisma <yolisisabel28@gmail.com>
2024-01-23 11:34:20 -0800

    rewrote html and added css

--- client/src/components/Courses.js
+++ client/src/components/Courses.js
@@ -12,16 +12,36 @@ function Courses() {
   }, []);
 
   return (
-    <div>
+    <div className="wrap main--grid">
       {courses.map((course) => (
-        <a key={course.id} href={`/courses/${course.id}`}>
-          <h2>Course</h2>
-          <h3>{course.title}</h3>
+        <a
+          key={course.id}
+          className="course--module course--link"
+          href={`/courses/${course.id}`}
+        >
+          <h2 className="course--label">Course</h2>
+          <h3 className="course--title">{course.title}</h3>
         </a>
       ))}
 
-      {/* need to include link to "Create Course" screen */}
-      <Link to="/courses/create">Create Course</Link>
+      <a
+        className="course--module course--add--module"
+        href="create-course.html"
+      >
+        <span className="course--add--title">
+          <svg
+            version="1.1"
+            xmlns="http://www.w3.org/2000/svg"
+            x="0px"
+            y="0px"
+            viewBox="0 0 13 13"
+            className="add"
+          >
+            <polygon points="7,6 7,0 6,0 6,6 0,6 0,7 6,7 6,13 7,13 7,7 13,7 13,6 " />
+          </svg>
+          New Course
+        </span>
+      </a>
     </div>
   );
 }
