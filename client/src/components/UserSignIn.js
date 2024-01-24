23a9ea716130f0a51324dccb7af849950ce84d30
Yolisma <yolisisabel28@gmail.com>
2024-01-24 15:17:45 -0800

    added className and main element

--- client/src/components/UserSignIn.js
+++ client/src/components/UserSignIn.js
@@ -41,31 +41,33 @@ const UserSignIn = () => {
     navigate("/");
   };
   return (
-    <div>
-      <h2>Sign In</h2>
-      <ErrorsDisplay errors={errors} />
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="emailAddress">Email Address</label>
-        <input
-          id="emailAddress"
-          name="emailAddress"
-          type="email"
-          ref={emailAddress}
-        />
-        <label htmlFor="password">Password</label>
-        <input id="password" name="password" type="password" ref={password} />
-        <button className="button" type="submit">
-          Sign In
-        </button>
-        <button className="button button-secondary" onClick={handleCancel}>
-          Cancel
-        </button>
-      </form>
-      <p>
-        Don't have a user account? Click here to{" "}
-        <Link to="/signup">sign up</Link>!
-      </p>
-    </div>
+    <main>
+      <div className="form--centered">
+        <h2>Sign In</h2>
+        <ErrorsDisplay errors={errors} />
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="emailAddress">Email Address</label>
+          <input
+            id="emailAddress"
+            name="emailAddress"
+            type="email"
+            ref={emailAddress}
+          />
+          <label htmlFor="password">Password</label>
+          <input id="password" name="password" type="password" ref={password} />
+          <button className="button" type="submit">
+            Sign In
+          </button>
+          <button className="button button-secondary" onClick={handleCancel}>
+            Cancel
+          </button>
+        </form>
+        <p>
+          Don't have a user account? Click here to{" "}
+          <Link to="/signup">sign up</Link>!
+        </p>
+      </div>
+    </main>
   );
 };
 
