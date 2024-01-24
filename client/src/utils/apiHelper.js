9d8f4df1096d52c1379b9a0c2382467d5d0fe514
Yolisma <yolisisabel28@gmail.com>
2024-01-24 15:58:37 -0800

    changed property of username to emailaddress for auth

--- client/src/utils/apiHelper.js
+++ client/src/utils/apiHelper.js
@@ -12,7 +12,7 @@ export const api = (path, method = "GET", body = null, credentials = null) => {
   }
   if (credentials) {
     const encodedCredentials = btoa(
-      `${credentials.username}:${credentials.password}`
+      `${credentials.emailAddress}:${credentials.password}`
     );
     options.headers.Authorization = `Basic ${encodedCredentials}`;
   }
